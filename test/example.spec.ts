3a4afeca05b7857f049fd470373fd37c50587002
Jeffesson Gomes <jeffessong4@gmail.com>
2023-05-12 15:01:31 -0300

    refactor: melhorando a estrutura do teste

--- test/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from 'vitest'
-
-test('o usuário consegue criar uma nova transação', () => {
-  const responseStatusCode = 201
-
-  expect(responseStatusCode).toEqual(201)
-})
